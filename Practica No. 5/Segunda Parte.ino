5065f84e343b3cabdfc24a4fb4521824da182179
lastorpablo <142429982+lastorpablo@users.noreply.github.com>
2024-05-09 07:41:01 -0600

    Segunda Parte.ino

--- Practica No. 5/Segunda Parte.ino
+++ Practica No. 5/Segunda Parte.ino
@@ -1,3 +1,16 @@
+/*
+   Fundacion Kinal
+   Centro educativo tecnico laboral Kinal
+   Quinto perito
+   Quinto electronica
+   Codigo Tecnico: EB5AM  
+   Curso: Taller de electronica digital y reparacion de computadoras I
+   Proyecto: Relevadores y Visualizadores
+   Dev: Kevin Owen Roger Pablo Lastor
+   Fecha: 02 de mayo del 2024
+*/
+
+
 #define sol 391.995
 #define mib 311.127
 #define sib 466.164
